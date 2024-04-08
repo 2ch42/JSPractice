409c2df05ed54f426b08e9409b7f578131385b89
Changhyeon Lee <changhyl@student.42seoul.kr>
2024-04-08 17:34:21 +0900

    file name, var name modified

--- js/game.js
+++ js/game.js
@@ -1,18 +1,18 @@
-const bar = document.querySelector("#bar");
-const ctx = bar.getContext("2d");
+const game = document.querySelector("#game");
+const ctx = game.getContext("2d");
 
-const BARTOP = 0;
-const BARBOTTOM = bar.height;
-const BARLEFT = 0;
-const BARRIGHT = bar.width;
+const GAMETOP = 0;
+const GAMEBOTTOM = game.height;
+const GAMELEFT = 0;
+const GAMERIGHT = game.width;
 
 const STICKWIDTH = 10;
 const STICKHEIGHT = 50;
 
-let x1 = BARLEFT;
-let y1 = bar.height / 2;
-let x2 = BARRIGHT - STICKWIDTH;
-let y2 = bar.height / 2;
+let x1 = GAMELEFT;
+let y1 = game.height / 2;
+let x2 = GAMERIGHT - STICKWIDTH;
+let y2 = game.height / 2;
 
 const keys = {
     ArrowUp: false,
@@ -21,8 +21,8 @@ const keys = {
     s: false
 };
 
-function drawBar() {
-        ctx.clearRect(0, 0, bar.width, bar.height);
+function drawGame() {
+        ctx.clearRect(0, 0, game.width, game.height);
         ctx.fillStyle = "black";
         ctx.fillRect(x1, y1, STICKWIDTH, STICKHEIGHT);
         ctx.fillRect(x2, y2, STICKWIDTH, STICKHEIGHT);
@@ -37,8 +37,6 @@ document.addEventListener("keydown", function(event) {
 });
 
 document.addEventListener("keyup", function(event) {
-    // 해당 키의 상태를 false로 설정
-    // keys[event.key] = false;
     if (event.key === "w") {
         keys[event.key] = false;
     }
@@ -52,44 +50,39 @@ document.addEventListener("keyup", function(event) {
         keys[event.key] = false;
     }
 
-    // 모든 키의 상태를 확인하여 물체의 움직임 조절
     moveObjects();
 });
 
 function moveObjects() {
-    // ArrowUp 또는 w 키가 눌려있을 때
     if (keys.w) {
         y1 -= 5;
-        if (y1 <= BARTOP) {
-            y1 = BARTOP;
-        }
+        if (y1 <= GAMETOP) {
+            y1 = GAMETOP;GAMEBOTTOM
+        }GAMEBOTTOM
     }
-    // ArrowDown 또는 s 키가 눌려있을 때
     if (keys.s) {
         y1 += 5;
-        if (y1 >= (BARBOTTOM - STICKHEIGHT)) {
-            y1 = BARBOTTOM - STICKHEIGHT;
+        if (y1 >= (GAMEBOTTOM - STICKHEIGHT)) {
+            y1 = GAMEBOTTOM - STICKHEIGHT;
         }
     }
-    // ArrowUp 또는 w 키가 눌려있을 때
     if (keys.ArrowUp) {
         y2 -= 5;
-        if (y2 <= BARTOP) {
-            y2 = BARTOP;
-        }
+        if (y2 <= GAMETOP) {
+            y2 = GAMETOP;GAMEBOTTOM
+        }GAMEBOTTOM
     }
-    // ArrowDown 또는 s 키가 눌려있을 때
     if (keys.ArrowDown) {
         y2 += 5;
-        if (y2 >= (BARBOTTOM - STICKHEIGHT)) {
-            y2 = BARBOTTOM - STICKHEIGHT;
+        if (y2 >= (GAMEBOTTOM - STICKHEIGHT)) {
+            y2 = GAMEBOTTOM - STICKHEIGHT;
         }
     }
 
-    drawBar();
+    drawGame();
 }
 
-setInterval(drawBar, 10);
+setInterval(drawGame, 10);
 setInterval(moveObjects, 10);
 
-drawBar();
+drawGame();
