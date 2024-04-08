3095220277fdba9d7a8041dba4167c00b4b83fd0
Changhyeon Lee <changhyl@student.42seoul.kr>
2024-04-08 20:14:35 +0900

    Moving Ball Object created

--- js/game.js
+++ js/game.js
@@ -1,18 +1,33 @@
 const game = document.querySelector("#game");
 const ctx = game.getContext("2d");
 
+const BORDERWIDTH = 10;
+
 const GAMETOP = 0;
 const GAMEBOTTOM = game.height;
 const GAMELEFT = 0;
 const GAMERIGHT = game.width;
 
 const STICKWIDTH = 10;
-const STICKHEIGHT = 50;
+const STICKHEIGHT = 80;
 
+const BALLRADIUS = 13;
+
+// stick coordinate
 let x1 = GAMELEFT;
-let y1 = game.height / 2;
+let y1 = game.height / 2 - STICKHEIGHT / 2;
 let x2 = GAMERIGHT - STICKWIDTH;
-let y2 = game.height / 2;
+let y2 = game.height / 2 - STICKHEIGHT / 2;
+
+// ball coordinate
+let ballX = GAMERIGHT / 2;
+let ballY = GAMEBOTTOM / 2;
+
+// let speedX = Math.random;
+// let speedY = Math.random;
+
+let speedX = 1.5;
+let speedY = 1.5;
 
 const keys = {
     ArrowUp: false,
@@ -22,11 +37,53 @@ const keys = {
 };
 
 function drawGame() {
-        ctx.clearRect(0, 0, game.width, game.height);
-        ctx.fillStyle = "black";
-        ctx.fillRect(GAMERIGHT / 2, 0, 5, GAMEBOTTOM);
-        ctx.fillRect(x1, y1, STICKWIDTH, STICKHEIGHT);
-        ctx.fillRect(x2, y2, STICKWIDTH, STICKHEIGHT);
+    ctx.clearRect(0, 0, game.width, game.height); // center line
+    ctx.fillStyle = "black";
+    ctx.fillRect(GAMERIGHT / 2, 0, 5, GAMEBOTTOM);
+    ctx.fillRect(x1, y1, STICKWIDTH, STICKHEIGHT); // stick1
+    ctx.fillRect(x2, y2, STICKWIDTH, STICKHEIGHT); // stick2
+    drawBall();
+}
+
+function drawBall()
+{
+    ctx.beginPath();
+    ctx.strokeStyle = "orange";
+    ctx.fillStyle = "orange";
+    ctx.arc(ballX, ballY, BALLRADIUS, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.stroke();
+}
+
+function moveBall() {
+    ballX += speedX;
+    ballY += speedY;
+    if (ballX <= x1 + BALLRADIUS + STICKWIDTH) // left wall or stick
+    {
+        /*
+        left wall: x1 + BALLRADIUS
+
+        left stick: x1 + BALLRADIUS + STICKWIDTH
+        */
+        speedX *= -1;
+    }
+    else if (ballX >= x2 - BALLRADIUS) // right wall or stick 
+    {
+        /* 
+        right wall: x2
+
+        right stick: x2 - BALLRADIUS
+        */
+        speedX *= -1;
+    }
+    else if (ballY <= 0 + BALLRADIUS) // top wall
+    {
+        speedY *= -1;
+    }
+    else if (ballY >= GAMEBOTTOM - BALLRADIUS) // bottom wall
+    {
+        speedY *= -1;
+    }
 }
 
 document.addEventListener("keydown", function(event) {
@@ -34,7 +91,7 @@ document.addEventListener("keydown", function(event) {
     keys[event.key] = true;
 
     // 모든 키의 상태를 확인하여 물체의 움직임 조절
-    moveObjects();
+    moveSticks();
 });
 
 document.addEventListener("keyup", function(event) {
@@ -51,10 +108,10 @@ document.addEventListener("keyup", function(event) {
         keys[event.key] = false;
     }
 
-    moveObjects();
+    moveSticks();
 });
 
-function moveObjects() {
+function moveSticks() {
     if (keys.w) {
         y1 -= 5;
         if (y1 <= GAMETOP) {
@@ -70,8 +127,8 @@ function moveObjects() {
     if (keys.ArrowUp) {
         y2 -= 5;
         if (y2 <= GAMETOP) {
-            y2 = GAMETOP;GAMEBOTTOM
-        }GAMEBOTTOM
+            y2 = GAMETOP;
+        }
     }
     if (keys.ArrowDown) {
         y2 += 5;
@@ -84,6 +141,7 @@ function moveObjects() {
 }
 
 setInterval(drawGame, 10);
-setInterval(moveObjects, 10);
+setInterval(moveSticks, 10);
+setInterval(moveBall, 10);
 
 drawGame();
